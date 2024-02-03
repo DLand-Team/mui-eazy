b8d41579cf953e9c7352277cf46ef4c1187321a7
qanglee <545540710@qq.com>
2024-02-03 21:06:58 +0800

    feat: 发版

--- packages/mui-eazy-spilt/rollup.config.js
+++ packages/mui-eazy-spilt/rollup.config.js
@@ -4,23 +4,12 @@ import image from "@rollup/plugin-image";
 import json from "@rollup/plugin-json";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
-// import * as fs from "fs";
-// import * as path from "path";
-// import { RollupOptions } from "rollup";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
 import { terser } from "rollup-plugin-terser";
+
 /**entries */
 const entry = "src/index.ts";
-// const componentsDir = "src/components";
-// const componentsName = fs.readdirSync(path.resolve(componentsDir));
-// const componentsEntry = componentsName
-//   .filter((item) => {
-//     let path = `${componentsDir}/${item}`;
-//     return fs.statSync(path).isDirectory();
-//   })
-//   .map((name) => `${componentsDir}/${name}/index.ts`);
-
 const isProd = process.env.NODE_ENV === "production";
 
 const commonPlugins = [

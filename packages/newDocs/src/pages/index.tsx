660b45bf1c3e1416651d87cfd38f9c4ad931c352
qanglee <545540710@qq.com>
2024-02-05 13:39:36 +0800

    dev

--- packages/newDocs/src/pages/index.tsx
+++ packages/newDocs/src/pages/index.tsx
@@ -2,27 +2,15 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import Logo from "@site/static/img/logo.svg";
 import Layout from "@theme/Layout";
-import styles from "./index.module.css";
+import styles from "./index.module.scss";
 function HomepageHeader() {
 	const { siteConfig } = useDocusaurusContext();
-	debugger
+	debugger;
 	return (
 		<header className={styles.heroBanner}>
 			<div className="container">
-				<div
-					style={{
-						display: "flex",
-						height: "500px",
-						paddingTop: "30px",
-					}}
-				>
-					<div
-						style={{
-							textAlign: "left",
-							flex: 1,
-							height: "100%",
-						}}
-					>
+				<div className={styles.header}>
+					<div className={styles.infoPart}>
 						<h1>
 							<p className={styles.title}>MuiEazy</p>
 						</h1>
@@ -38,13 +26,7 @@ function HomepageHeader() {
 						</h1>
 						<div className={styles.btn}>Get Started</div>
 					</div>
-					<div
-						style={{
-							flex: 1,
-							height: "100%",
-							transform: "translate(0px,50px)",
-						}}
-					>
+					<div className={styles.logoPart}>
 						<div className={styles.image}>
 							<div className={styles.imageContainer}>
 								<div className={styles.imageBg}></div>

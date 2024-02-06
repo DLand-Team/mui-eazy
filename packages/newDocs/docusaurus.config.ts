25db696e9d4364b7a94e904b41603d0d8ee89a72
qanglee <545540710@qq.com>
2024-02-06 09:22:11 +0800

    feat: search i18n

--- packages/newDocs/docusaurus.config.ts
+++ packages/newDocs/docusaurus.config.ts
@@ -24,9 +24,25 @@ const config: Config = {
 	// may want to replace "en" with "zh-Hans".
 	i18n: {
 		defaultLocale: "en",
-		locales: ["en"],
+		locales: ["en", "zh"],
+		path: "i18n",
+		localeConfigs: {
+			en: {
+				label: "English",
+				direction: "ltr",
+				htmlLang: "en-US",
+				calendar: "gregory",
+				path: "en",
+			},
+			zh: {
+				label: "中文",
+				direction: "ltr",
+				htmlLang: "zh-CN",
+				calendar: "gregory",
+				path: "zh",
+			},
+		},
 	},
-
 	presets: [
 		[
 			"classic",
@@ -53,6 +69,35 @@ const config: Config = {
 	],
 	themes: ["@docusaurus/theme-live-codeblock"],
 	themeConfig: {
+		algolia: {
+			// The application ID provided by Algolia
+			appId: "YOUR_APP_ID",
+
+			// Public API key: it is safe to commit it
+			apiKey: "YOUR_SEARCH_API_KEY",
+
+			indexName: "YOUR_INDEX_NAME",
+
+			// Optional: see doc section below
+			contextualSearch: true,
+
+			// Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+			externalUrlRegex: "external\\.com|domain\\.com",
+
+			// Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+			replaceSearchResultPathname: {
+				from: "/docs/", // or as RegExp: /\/docs\//
+				to: "/",
+			},
+
+			// Optional: Algolia search parameters
+			searchParameters: {},
+
+			// Optional: path for search page that enabled by default (`false` to disable it)
+			searchPagePath: "search",
+
+			//... other Algolia params
+		},
 		colorMode: {
 			defaultMode: "dark",
 		},
@@ -65,6 +110,10 @@ const config: Config = {
 				src: "img/logo.svg",
 			},
 			items: [
+				{
+					type: "localeDropdown",
+					position: "right",
+				},
 				{
 					type: "docSidebar",
 					sidebarId: "tutorialSidebar",
